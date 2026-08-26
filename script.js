7c9282bc058b5025a8ab0e056fff12b15329e9c9
Pedro Silva <pedrosilva10057@gmail.com>
2026-08-25 21:16:16 -0300

    feat: updated monitor password acess

--- script.js
+++ script.js
@@ -74,8 +74,8 @@ function formatDataCriacao(timestamp) {
 // --------------------------------------------------------------------------
 // CONFIG
 // --------------------------------------------------------------------------
-const ADMIN_PASSWORD       = "casemiro2026";
-const MONITOR_PASSWORD     = null; // null = aceita qualquer senha
+const ADMIN_PASSWORD       = "sup.admin";
+const MONITOR_PASSWORD     = "monitori@123"; // TODO: trocar pela senha real da monitoria
 const RH_PASSWORD          = "rh2026"; // TODO: trocar pela senha real do setor de RH
 const HEARTBEAT_INTERVALO  = 25_000;
 const HEARTBEAT_EXPIRACAO  = 75_000;
